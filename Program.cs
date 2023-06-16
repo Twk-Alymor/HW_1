28d87ec0df65fa332c7aff166e0c1f8eaa8fb84f
Twk-Alymor <101538257+Twk-Alymor@users.noreply.github.com>
2023-06-16 13:44:07 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// To Calculate clock angle
+static double clockAngle(int hr, int min)
+{
+    double minAngle = min * 360 / 60;
+    double hrAngle = 0;
+
+    if (hr != 12)
+    {
+        hrAngle = (hr * 360) / 12;
+    }
+
+    double diffAngle = Math.Abs(hrAngle - minAngle);
+    double lesserAngle = Math.Min(diffAngle, 360 - diffAngle);
+
+    return lesserAngle;
+}
+
+// Input hour (1-12)
+Console.Write("Enter Hour: ");
+int hr = Convert.ToInt32(Console.ReadLine());
+
+//Input minute (00 - 59)
+Console.Write("Enter Minute: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+double angle = clockAngle(hr, min);
+Console.WriteLine("The lesser angle between hour and minute hands is: " + angle + "°.");
+
+// Exit
+Console.Write("Press any key to close the console app..");
+Console.ReadKey();
